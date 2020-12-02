ffd7063f4fd89e4dca73c46c228cf5f184e6bae1
LjCodeZA <75343843+LjCodeZA@users.noreply.github.com>
2020-12-02 21:56:29 +0200

    Moved project to Github
    Moved project to Github

---
+++ OnlineStore/Controllers/ProductDashboardController.cs
@@ -0,0 +1,43 @@
+﻿using OnlineStore.DAL;
+using OnlineStore.Models;
+using OnlineStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    public class ProductDashboardController : Controller
+    {
+        // GET: ProductDashboard
+        public ActionResult Index()
+        {
+            var dbContext = new OnlineStoreContext();
+            var productDashboardViewModelList = new List<ProductDashboardViewModel>();
+
+            var productDashboard = (from product in dbContext.Product select product);
+
+            foreach (var item in productDashboard)
+            {
+                var productItem = new ProductDashboardViewModel();
+                productItem.ProductId = item.ProductId;
+                productItem.Name = item.Name;
+                productItem.Description = item.Description;
+                productItem.ImageURL = item.ImageURL;
+                productItem.ListPrice = (from product in dbContext.ProductVendor where product.ProductId == item.ProductId select product.ListPrice).FirstOrDefault();
+
+                productDashboardViewModelList.Add(productItem);
+            }
+            
+            return View(productDashboardViewModelList);
+        }
+
+        public int CartCheckout(string cartData)
+        {
+            Console.WriteLine(cartData);
+            return 1;
+        }
+    }
+}
